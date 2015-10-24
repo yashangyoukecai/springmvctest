75144f74e1f30f4781b1efe1093c6c280dd6a3b0
jinwu <jinwu@mogujie.com>
2015-10-24 15:04:18 +0800

    commit

---
+++ src/main/java/com/springapp/mvc/HelloWorldLastModifiedCacheController.java
@@ -0,0 +1,27 @@
+package com.springapp.mvc;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+import org.springframework.web.servlet.mvc.LastModified;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by wklmogujie on 15/10/24.
+ */
+public class HelloWorldLastModifiedCacheController extends AbstractController implements LastModified {
+    private long lastModified;
+    protected ModelAndView handleRequestInternal(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+        //点击后再次请求当前页面
+        resp.getWriter().write("<a href=''>this</a>");
+        return null;
+    }
+    public long getLastModified(HttpServletRequest request) {
+        if(lastModified == 0L) {
+            //TODO 此处更新的条件：如果内容有更新，应该重新返回内容最新修改的时间戳
+            lastModified = System.currentTimeMillis();
+        }
+        return lastModified;
+    }
+}

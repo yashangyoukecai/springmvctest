3baa25e81e789c5b7de55733f34335b2b6cdde64
jinwu <jinwu@mogujie.com>
2015-10-27 11:28:53 +0800

    commit

--- src/main/java/com/wkl/Controllers/UserController.java
+++ src/main/java/com/wkl/Controllers/UserController.java
@@ -5,9 +5,12 @@ import com.wkl.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.Validator;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.annotation.Resource;
 
 /**
  * Created by wklmogujie on 15/10/26.
@@ -18,14 +21,27 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Resource
+    private Validator validator;
+
     @RequestMapping(value = "/login", method = RequestMethod.POST)
     protected String handle(UserModel user, BindingResult result, ModelAndView modelAndView) {
-        String flag = userService.userLogin(user);
-        if ("success".equals(flag)) {
+//        String flag = userService.userLogin(user);
+//        if ("success".equals(flag)) {
 //            Map<String, String> map = new HashMap<String, String>();
 //            map.put("name", user.getName());
 //            map.put("password", user.getPassword());
 //            return new ModelAndView("success", map);
+//            return "success";
+//        }
+        this.validator.validate(user, result);//添加验证
+        if (result.hasErrors()) {
+            modelAndView.addObject("user", user);// 把user对象返回到页面， 这样不至于表单被清空了
+            return "index";
+        }
+
+        String flag = userService.userLogin(user);
+        if ("success".equals(flag)) {
             return "success";
         }
         return "fail";
